5d78c2cd6587eeed82a8bcac6f3d260949fbc6e8
Yassine ben ameur <superyassine15@gmail.com>
2019-04-07 00:50:57 +0100

    added oauth login with facebook / google

---
+++ src/main/java/com/example/trippify/TrippifyApplication.java
@@ -0,0 +1,17 @@
+package com.example.trippify;
+
+import com.example.trippify.config.AppProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
+
+public class TrippifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrippifyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/trippify/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.trippify.controller;
+
+import com.example.trippify.exception.ResourceNotFoundException;
+import com.example.trippify.model.User;
+import com.example.trippify.repository.UserRepository;
+import com.example.trippify.security.CurrentUser;
+import com.example.trippify.security.UserPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/user/me")
+    @PreAuthorize("hasRole('USER')")
+    public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
+        return userRepository.findById(userPrincipal.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
+    }
+}
